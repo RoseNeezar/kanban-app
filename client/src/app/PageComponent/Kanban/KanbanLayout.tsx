1ad9bca0cf2a10ec542ea11c7b241bcf3e99b926
Neezar <rforlyffe@gmaicl.com>
2021-07-28 23:00:46 +0800

    update kanban page

--- client/src/app/PageComponent/Kanban/KanbanLayout.tsx
+++ client/src/app/PageComponent/Kanban/KanbanLayout.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useKanbanStore } from "../../stores/useKanbanStore";
 import Navigate from "../../utils/Navigate";
+import { useGetKanban } from "./hooks/useGetKanban";
 
 const KanbanLayout = () => {
-  const {
-    GetAllBoards,
-    kanbanBoards,
-    CreateBoard,
-    DeleteBoard,
-    setListInCurrentBoard,
-  } = useKanbanStore();
+  const { GetAllBoards, kanbanBoards, DeleteBoard, setListInCurrentBoard } =
+    useKanbanStore();
 
   const [boardTitle, setBoardTitle] = useState("");
+
+  const { FetchKanban, KanbanLoading } = useGetKanban(boardTitle);
+
   useEffect(() => {
     GetAllBoards();
   }, []);
@@ -21,9 +20,16 @@ const KanbanLayout = () => {
     Navigate?.push(`/board/${boardId}`);
   };
   const HandleAddBoard = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.keyCode === 13) {
-      CreateBoard(boardTitle);
-
+    if (boardTitle.length > 0) {
+      if (e.key === "Enter") {
+        FetchKanban();
+        setBoardTitle("");
+      }
+    }
+  };
+  const HandleAddBoardButton = () => {
+    if (boardTitle.length > 0) {
+      FetchKanban();
       setBoardTitle("");
     }
   };
@@ -32,21 +38,30 @@ const KanbanLayout = () => {
   };
   return (
     <>
-      {!kanbanBoards ? (
+      {KanbanLoading ? (
         <h1>Loading...</h1>
       ) : (
-        <div className="flex flex-col items-center justify-center w-full mt-10">
-          <p className="text-lg text-dark-txt">
-            Fill the input with a board name, then press 'Enter'
-          </p>
-          <input
-            className="p-2 mt-8 mb-8 rounded-md w-96"
-            value={boardTitle}
-            onChange={(e) => setBoardTitle(e.target.value)}
-            onKeyDown={(e) => HandleAddBoard(e)}
-          />
-          <div className="grid justify-center w-full grid-flow-row gap-10 auto-rows-min grid-rows-min grid-cols-fit">
-            {kanbanBoards.boards
+        <div className="flex flex-col items-center justify-center w-full mt-10 overflow-hidden b ">
+          <div className="flex flex-col items-center ">
+            <p className="text-2xl text-dark-txt">Create Board</p>
+            <div className="flex flex-row items-center">
+              <input
+                className="p-2 mt-8 mb-8 rounded-md w-96"
+                value={boardTitle}
+                onChange={(e) => setBoardTitle(e.target.value)}
+                onKeyDown={(e) => HandleAddBoard(e)}
+              />
+              <button
+                onClick={() => HandleAddBoardButton()}
+                className="w-20 h-10 ml-4 cursor-pointer hover:bg-dark-second rounded-xl bg-dark-third text-dark-txt"
+              >
+                Create
+              </button>
+            </div>
+          </div>
+
+          <div className="grid justify-center w-full grid-flow-row gap-10 overflow-scroll auto-rows-min grid-rows-min grid-cols-fit">
+            {kanbanBoards?.boards
               .filter((fil) => fil.title !== "")
               .map((res) => (
                 <div className="p-2 rounded-md bg-dark-third" key={res._id}>
@@ -60,7 +75,7 @@ const KanbanLayout = () => {
                   </div>
 
                   <button
-                    className="w-full rounded-md h-60 hover:text-black hover:bg-gray-200 bg-dark-second text-dark-txt"
+                    className="w-full h-20 rounded-md hover:text-black hover:bg-gray-200 bg-dark-second text-dark-txt"
                     onClick={() => HandleBoard(res._id)}
                   >
                     {res.title}
